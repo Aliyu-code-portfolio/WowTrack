0ad0990aa103b5495eb7fe3f5935f4755cb40476
AllyCodeX <aliyuabdullahi074@outlook.com>
2022-07-18 16:48:18 +0100

    Initial Push

---
+++ Interactive/Program.cs
@@ -0,0 +1,96 @@
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Interactive
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            //Application.Run(new Form1());
+            Interact.AcceptTerms();
+
+        }
+
+    }
+    public class Interact
+    {
+        public static void AcceptTerms()
+        {
+            DialogResult dialogResult = MessageBox.Show("By clicking below, you have accepted the charges attached to using this Wowbudd", "Accept Terms of Use", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                try
+                {
+                    //SendEmail().Wait();
+                    SaveToDB("Yes wow");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+
+
+        static async Task SendEmail()
+        {
+            //var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            //read data from a file stored from C drive/windows
+            var info = GetModelAndClient();
+            string clientName = info[0];
+            var model = info[1];
+            int minutes = 234;
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("aliyu.abdullahi@equipmenthall.com", "Aliyu Abdullahi");
+            var subject = $"WowBudd model {model} has been used recently";
+            var to = new EmailAddress("harryalex9821@gmail.com", "Harry Alex");
+            var plainTextContent = $"Wowbudd with model number: {model} was used for {minutes} minutes by {clientName}. This is an automated mail to alert the company of wowbudd usage, Please do not reply to this email ";
+            var htmlContent = $"<strong>Wowbudd with model number: <b>{model}</b> was used for <b>{minutes}</b> minutes by <b>{clientName}</b>. This is an automated mail to alert the company of wowbudd usage, Please do not reply to this email  </strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            //Setup database next
+        }
+        static string[] GetModelAndClient()
+        {
+            try
+            {
+                int counter = 0;
+                string[] info;
+                info = new string[2];
+                foreach (string line in System.IO.File.ReadLines(@"c:\Windows\Web\info.txt"))
+                {
+                    info[counter] = line;
+                    counter++;
+                }
+
+                return info;
+            }
+            catch (IOException)
+            {
+                string[] info;
+                info = new string[2];
+                info[0] = "Can not find client name";
+                info[1] = "Cannot find model number, please check if a file was deleted from the web folder of the wowbudd the wowbudd ";
+                return info;
+            }
+        }
+        static async void SaveToDB(string pen)
+        {
+
+        }
+
+    }
+}
---
+++ WowTrack/WowTrack.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+using System.Threading;
+
+
+namespace WowTrack
+{
+    [RunInstaller(true)]
+    public partial class WowTrack : ServiceBase
+    {
+        int scheduleTime = Convert.ToInt32(ConfigurationSettings.AppSettings["ThreadTime"]);
+        public Thread worker = null;
+        public WowTrack()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                ThreadStart start = new ThreadStart(Working);
+                worker = new Thread(start);
+                worker.Start();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+        public void Working()
+        {
+            StartZoomLoop();
+        }
+
+        public static void StartZoomLoop()
+        {
+            //Checks if zoom is currently running
+            Process[] list = Process.GetProcessesByName("zoom");
+            if (list.Length < 1)
+            {
+                // Recheck zoom if is not running
+                string path = "C:\\wowtrack.txt";
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine(string.Format("The tracking has started... "+DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")+" No zoom running, checking again in 3 seconds"));
+                    writer.Close();
+                }
+                int sleepTime = 3000; //60000; // in mills
+                Task.Delay(sleepTime).Wait();
+                // or
+                //Thread.Sleep(sleepTime);
+                StartZoomLoop();
+            }
+            else
+            {
+                // Executes if zoom is running
+                try
+                {
+
+                    foreach (Process p in list)
+                    {
+                        p.Kill();
+                        //Write a Dialog message
+                        //asking if the user accepts Wowbii's terms of usage and charges associated with it,
+                        //if No then close all Zoom processes and startzoomloop again
+                        //if Yes then calculate how long zoom is active for and send it after sending a log to the database
+
+
+                        ///////////////////
+                        //Process[] cam = Process.GetProcessesByName("camera");
+                        //checking if camera and audio are in use
+                    }
+                    StartZoomLoop();
+                }
+                catch (Exception e)
+                {
+                    string path = "C:\\wowtrack.txt";
+                    using (StreamWriter writer = new StreamWriter(path, true))
+                    {
+                        writer.WriteLine(string.Format("Error code "+e));
+                        writer.Close();
+                    }
+                    StartZoomLoop();
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                if ((worker != null) & worker.IsAlive)
+                {
+                    worker.Abort();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
